6b1f4e0d79b068f98f97fd09bc4463a3237b0d89
Asif <2430012@cognizant.com>
2025-09-18 14:50:54 +0530

    Modify start-script

--- start-servers.bat
+++ start-servers.bat
@@ -10,11 +10,11 @@ echo [2/4] Starting Config Server (Port 9091)...
 start "Config-Server" cmd /k "cd config-server && mvn spring-boot:run"
 timeout /t 10 /nobreak >nul
 
-echo [3/4] Starting Auth Server (Port 9090)...
+echo [3/4] Starting Gateway Server (Port 9090)...
 start "Gateway-Server" cmd /k "cd gateway-server && mvn spring-boot:run"
 timeout /t 10 /nobreak >nul
 
-echo [4/4] Starting Gateway Server (Port 9093)...
+echo [4/4] Starting Auth Server (Port 9093)...
 start "Auth-Server" cmd /k "cd auth-server && mvn spring-boot:run"
 
 echo.
@@ -22,8 +22,8 @@ echo All servers are starting up!
 echo.
 echo Access your application at:
 echo - Gateway Server: http://localhost:9090
-echo - Discovery Server: http://localhost:9092
 echo - Config Server: http://localhost:9091
+echo - Discovery Server: http://localhost:9092
 echo - Auth Server: http://localhost:9093
 echo.
 echo Press any key to exit this window...
