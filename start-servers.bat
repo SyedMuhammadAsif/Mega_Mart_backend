a82a26a1fdcbfd8856888c8e0ab523b770fe6175
Asif <2430012@cognizant.com>
2025-09-18 14:48:53 +0530

    Change order of server running

--- start-servers.bat
+++ start-servers.bat
@@ -10,12 +10,12 @@ echo [2/4] Starting Config Server (Port 9091)...
 start "Config-Server" cmd /k "cd config-server && mvn spring-boot:run"
 timeout /t 10 /nobreak >nul
 
-echo [3/4] Starting Auth Server (Port 9093)...
-start "Auth-Server" cmd /k "cd auth-server && mvn spring-boot:run"
+echo [3/4] Starting Auth Server (Port 9090)...
+start "Gateway-Server" cmd /k "cd gateway-server && mvn spring-boot:run"
 timeout /t 10 /nobreak >nul
 
-echo [4/4] Starting Gateway Server (Port 9090)...
-start "Gateway-Server" cmd /k "cd gateway-server && mvn spring-boot:run"
+echo [4/4] Starting Gateway Server (Port 9093)...
+start "Auth-Server" cmd /k "cd auth-server && mvn spring-boot:run"
 
 echo.
 echo All servers are starting up!
@@ -24,6 +24,7 @@ echo Access your application at:
 echo - Gateway Server: http://localhost:9090
 echo - Discovery Server: http://localhost:9092
 echo - Config Server: http://localhost:9091
+echo - Auth Server: http://localhost:9093
 echo.
 echo Press any key to exit this window...
 pause >nul
